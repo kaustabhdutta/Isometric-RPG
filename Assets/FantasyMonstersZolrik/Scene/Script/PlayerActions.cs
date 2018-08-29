ef15949c66ec3e5d00e9caca085772b71d8c03da
ns7767@rit.edu <Naveen@DESKTOP-794QGAI>
2018-08-29 19:46:15 -0400

    player movement and action

---
+++ Assets/FantasyMonstersZolrik/Scene/Script/PlayerActions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    public Animator anim;
+    public float turnSpeed = 75;
+    public float moveSpeed = 5f;
+     
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        //player = GetComponent<GameObject>(); 
+	} 
+   private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            anim.Play("Walk");
+        }           
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+            anim.Play("Walk");
+        }            
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+            anim.Play("Walk");
+        }            
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+            anim.Play("Walk");
+        }
+        
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.Play("Attack");
+            anim.SetBool("Attack", false);
+        }
+    }
+}
