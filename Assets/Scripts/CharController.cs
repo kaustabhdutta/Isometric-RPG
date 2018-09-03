4307c509683e87d4f81de9f65356a0def34ddecd
ns7767@rit.edu <Naveen@DESKTOP-794QGAI>
2018-09-03 19:05:42 -0400

    Character movement with WASD Keys

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 4.0f;
+
+    Vector3 forward, right;
+    Animator anim;
+	void Start ()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0,90,0)) * forward;
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.anyKey)
+            move();
+	}
+    void move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"),0,Input.GetAxis("VerticalKey"));
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
+        anim.Play("Walk");
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.Play("Attack");
+        }
+    }
+}
